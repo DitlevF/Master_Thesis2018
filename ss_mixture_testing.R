54851fa54c7aafb5e244e81a85382d3fa54e1cf7
DitlevF <32267864+DitlevF@users.noreply.github.com>
2018-10-05 10:05:34 +0200

    Add files via upload
    Code for testing ss_mixture function.

---
+++ ss_mixture_testing.R
@@ -0,0 +1,42 @@
+# Test mixture algorithm
+
+rm(list=ls())
+library(coda)
+
+# Load working directory either at home or at CSS
+if(substring(getwd(),0,1) == 'C'){
+  setwd('C:/Users/fhk798/OneDrive/R Codes/') # PC
+} else{
+  setwd('/Users/ditlevkf/OneDrive/R Codes/') # Mac
+}
+
+# Load Data
+
+source('Generate Models/gen_std_linmod_bch.R')
+
+# Load data II: a la BCH
+nobs <- 100
+kvar <- 50 # Must be larger than 25
+
+df <- gen_std_linmod_bch(n = nobs, k = kvar, alpha_0 = 1, sigma_zeta = 1, sigma_v = 1 )
+d <- df$d; x <- df$X; Y <- df$Y
+X <- as.matrix(cbind(d,x))
+
+beta_true <- c(rep(1,6),rep(0,5),rep(1,5),rep(0,kvar-15)) # TRUE/FALSE vector of regressors.
+
+draws <- ss_mixture(X,Y, iter = 1000, c = 10, fix = c(0,0,0,1, rep(0,kvar-3))); # Note, higher c, higher exclusion probability.
+
+alphas <- draws$alphas
+
+deltas <- draws$deltas
+mu <- draws$mu
+sigma2 <- draws$sigma2
+omega <- draws$omega
+z <- draws$z
+
+# Evaluate MCMC
+post_alpha <- as.mcmc(alphas)
+plot(post_alpha)
+
+delta_mat <- as.matrix(deltas)
+colMeans(delta_mat)
